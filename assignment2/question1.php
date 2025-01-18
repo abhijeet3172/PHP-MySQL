9eccbbf1ad037a7a9fea927695d4df2802c22d61
abhijeet3172 <abhijeet.d.pawar317@gmail.com>
2025-01-18 22:52:09 +0530

    Add files via upload

---
+++ assignment2/question1.php
@@ -0,0 +1,25 @@
+<html>
+    <body bgcolor="#d1dee8">
+        <header>
+            <h2>Q1:</h2>
+        </header>
+    <?php
+    $sum = 0;
+
+// Loop through numbers from 1 to 100
+for ($i = 0; $i <= 100; $i++) {
+    
+    // Check if the number is even
+    if ($i % 2 == 0) {
+
+        // Add the even number to the sum
+        $sum += $i;
+    }
+}
+echo "The sum of all even numbers between 1 and 100 is: " . $sum;
+echo "<hr>";
+?>
+
+
+</body>
+</html>
